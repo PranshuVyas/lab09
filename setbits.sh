f64fb2b0ca2e7a0f37d902ab406fe099473ac5b5
Pranshu Vyas <38761690+PranshuVyas@users.noreply.github.com>
2018-11-26 10:51:28 -0500

    Update setbits.sh

--- setbits.sh
+++ setbits.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
-#Pin 0 RED 1 GREEN 2 YELLOW 3 BLUE
-toBinary(){
-    local n bit
-    for (( n=$1 ; n>0 ; n >>= 1 )); do  bit="$(( n&1 ))$bit"; done
-    printf "%s\n" "$bit"
-}
 
-echo "Enter a value between 0-15" 
-echo "The value entered is $1"
-echo toBinary $1
-if [ $1 gt 15 ]
-then
-        echo "Error number is not within the range of 0 - 15"
-fi
-for value in {0..3}
-do
-  gpio mode $value out
-done
-if (($1 & 1))
-then
-   gpio write 0 1
-else
-   gpio write 0 0
-fi
+in=$1
 
-if ((($1 >> 1) & 1 ))
-then
-   gpio write 1 1
-else
-   gpio write 1 0
-fi
 
-if ((($1 >> 2) & 1))
-then
-	gpio write 2 1
-else
-	gpio write 2 0
-fi
+a=$(($in&1))
+b=$((($in>>1)&1))
+c=$((($in>>2)&1))
+d=$((($in>>3)&1))
 
-if ((($1 >>3) & 1))
-then
-   gpio write 3 1
-else
-   gpio write 3 0
-fi 
+
+gpio write 0 $a
+gpio write 1 $b
+gpio write 2 $c
+gpio write 3 $d
+
+echo $a $b $c $d
